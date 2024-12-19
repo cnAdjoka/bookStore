0bdad8ec227c86430672e00390694e2ff47dd18e
cnAdjoka <ragequit078@gmail.com>
2024-12-19 16:54:10 -0500

    Premier Commit

---
+++ src/main/java/client.java
@@ -0,0 +1,6 @@
+public class client{
+    String name, address;
+    short age;
+
+    public client( String name, short age)
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.bookstore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.bookstore to javafx.fxml;
+    exports org.example.bookstore;
+}
